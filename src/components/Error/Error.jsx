ec6f8e6521ca0118cbdc6b4f1f53b47f18141576
SoloProgrammer <prathamshinde987@gmail.com>
2024-01-21 20:30:35 +0530

    Crreted resuable ErrorBlock component that shows error and again button

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Notfound from "@/components/NotFound/Notfound";
 import { NOT_FOUND } from "@/helpers/ErrorHandler";
+import styles from "./error.module.css";
 
 const ErrorPage = ({ error, reset }) => {
   if (error.message === NOT_FOUND) {
@@ -21,3 +22,12 @@ const ErrorPage = ({ error, reset }) => {
 };
 
 export default ErrorPage;
+
+export const ErrorBlock = ({ soure, refetch }) => {
+  return (
+    <div className={styles.errorBox}>
+      <h2>Unable to get {soure} at the moment!</h2>
+      <button onClick={refetch}>Try again</button>
+    </div>
+  );
+};
