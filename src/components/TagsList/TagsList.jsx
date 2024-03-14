a04ff8eeea4d086af04f3e6c2fe228ddd6133574
SoloProgrammer <prathamshinde987@gmail.com>
2024-03-15 00:41:45 +0530

    added sliding animations on tags component

--- src/components/TagsList/TagsList.jsx
+++ src/components/TagsList/TagsList.jsx
@@ -3,15 +3,18 @@ import chroma from "chroma-js";
 import Link from "next/link";
 import React from "react";
 import styles from "./tagsList.module.css";
+import { motion } from "framer-motion";
+import { anim } from "@/utils/framer";
 
 const TagsList = ({ tags, size = "small", isColored = true }) => {
   return (
     <div className={styles.tagsWrapper}>
-      {tags.map((tag) => {
+      {tags.map((tag, i) => {
         const color = chroma(tag.clr);
         return (
           <Link key={tag.id} href={`/posts?tag=${tag.slug}`}>
             <TagChip
+              i={i}
               tag={tag}
               color={color}
               size={size}
@@ -24,14 +27,32 @@ const TagsList = ({ tags, size = "small", isColored = true }) => {
   );
 };
 
-export const TagChip = ({ tag, color, size, isColored }) => {
+export const TagChip = ({ tag, color, size, isColored, i }) => {
   const SIZES = {
     small: "12px",
     medium: "14px",
     large: "16px",
   };
+
+  const slide = {
+    initial: {
+      opacity: 0,
+      x: -(i * 10),
+    },
+    inview: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring", // You can use different types of transitions
+        delay: i * 0.1, // Adjust duration as needed
+      },
+    },
+  };
+  
   return (
-    <span
+    <motion.span
+      {...anim(slide)}
+      viewport={{ once: true }}
       className={styles.tag}
       style={{
         "--color": isColored ? color : "var(--softTextColor)",
@@ -42,7 +63,7 @@ export const TagChip = ({ tag, color, size, isColored }) => {
       }}
     >
       #{tag.slug}
-    </span>
+    </motion.span>
   );
 };
 
