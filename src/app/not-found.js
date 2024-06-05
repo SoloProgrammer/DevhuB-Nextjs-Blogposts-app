64f1a00af2835e9189d599619cd63d89c9c5008d
SoloProgrammer <prathamshinde987@gmail.com>
2024-06-05 14:08:11 +0530

    removed use-client directive from 404 page file

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,8 @@
-"use client"
 import Notfound from "@/components/NotFound/Notfound";
 import React from "react";
 
 const page = () => {
-  return <Notfound/>;
+  return <Notfound />;
 };
 
 export default page;
